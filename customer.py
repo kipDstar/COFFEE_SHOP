f591f7838324562b9523aa8df8e22cedaff8ce96
Jesse Kipsang <kipsangjesse17@gmail.com>
2025-05-16 08:57:45 +0300

    completed all project test files, moving to creating test cases to validate code

--- customer.py
+++ customer.py
@@ -7,6 +7,9 @@ class Customer:
             raise TypeError("The customer name must be a string.")
         if not 1 <= len(name) <= 50:
             raise ValueError("The customer name must be between 1 and 50 characters long.")
+        if not hasattr(self.__class__, 'all_customers'):
+            self.__class__.all_customers = []
+        self.__class__.all_customers.append(self)
         self.name = name
         self._orders = []
     
@@ -28,9 +31,6 @@ class Customer:
     def coffees(self):
         return list(set(order.coffee for order in self._orders))
     
-    def __repr__(self):
-        return f"<Customer(name='{self.name}')>"
-    
     def create_order(self, coffee, price):
         if not isinstance(coffee, Coffee):
             raise TypeError("Coffee must be a Coffee instance.")
@@ -40,3 +40,29 @@ class Customer:
             return ValueError("Price must be between 1.0 and 10.0.")
         order = Order(self, coffee, price)
         return order
+    
+    def __repr__(self):
+        return f"<Customer(name='{self.name}')>"
+    
+    @classmethod
+    def coffee_aficionado(cls, coffee):
+        if not isinstance(coffee, Coffee):
+            raise TypeError("Coffee must be a Coffee instance.")
+        aficionado = None
+        max_spent = 0
+
+        if not hasattr(cls, '_all_customers'):
+            cls._all_customers = []
+        cls._all_customers.append(cls)
+
+        for customer in cls._all_customers:
+            total_spent = 0
+            for order in customer._orders:
+                if order.coffee == coffee:
+                    total_spent += order.price
+        
+            if total_spent > max_spent:
+                max_spent = total_spent
+                aficionado = customer
+        return aficionado
+    
